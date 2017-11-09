2ceaf2b309e736223beb40bfca99a37b61acc552
surya Arigela <surya Arigela>
2017-11-08 22:23:25 -0600

    adding Angualr project inital commit By SURYA ARIGELA

---
+++ src/app/search-result/search-result.component.ts
@@ -0,0 +1,30 @@
+import { RootObject } from './../root.class';
+import { BusSearchService } from './../bus-search.service';
+import { Component, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.css']
+})
+export class SearchResultComponent implements OnInit {
+
+ subscription: Subscription;
+  results: any;
+root:RootObject;
+  constructor(private service: BusSearchService) {
+    this.subscription = BusSearchService.results$.subscribe(results => {
+      this.results = results;
+      console.log("results in Search ",this.results)
+    })
+  }
+
+  ngOnInit() {
+   var retrievedObject = localStorage.getItem('testObject');
+   this.root= JSON.parse(retrievedObject);
+console.log('retrievedObject: ', this.root.data.onwardflights);
+  }
+
+}
